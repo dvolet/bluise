d3c621ac67350675db0b8b668871a7179e9dac06
dvolet <151787874+dvolet@users.noreply.github.com>
2026-04-11 11:28:04 +0000

    Change app.run to listen on all interfaces

--- app.py
+++ app.py
@@ -200,7 +200,5 @@ def verify_payment():
         return jsonify({"status": "success"})
 
     return jsonify({"status": "failed"})
-
-# ----------------- RUN -----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
