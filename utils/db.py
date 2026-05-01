d7ed9d06188996396caa4664dbe3438d819d1186
dvolet <151787874+dvolet@users.noreply.github.com>
2026-05-01 08:01:58 +0000

    Fix database connection path in db.py

--- utils/db.py
+++ utils/db.py
@@ -3,8 +3,13 @@ import sqlite3
 # =========================
 # DATABASE CONNECTION
 # =========================
+import os
+
 def get_connection():
-    conn = sqlite3.connect("database.db")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "..", "database.db")
+
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
